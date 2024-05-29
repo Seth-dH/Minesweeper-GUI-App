146ea3457a20980dcd093007306da8c02111d0d1
Seth deHaan <78933122+Seth-dH@users.noreply.github.com>
2024-05-28 20:16:34 -0400

    Basic grid layout
    implmented the basic layout for the gameplay page

--- Minesweeper/GameplayMainPage.xaml.cs
+++ Minesweeper/GameplayMainPage.xaml.cs
@@ -1,17 +1,13 @@
+using System.Reflection;
+
 namespace Minesweeper;
 
 public partial class GameplayMainPage : ContentPage
 {
-	int count = 0;
 
 	public GameplayMainPage()
 	{
 		InitializeComponent();
 	}
 
-    private void CounterButton_Clicked(object sender, EventArgs e)
-    {
-		count++;
-		CounterButton.Text = $"Clicked: {count} times!";
-    }
 }
